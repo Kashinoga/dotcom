e9e76baed4e89421bc350bedda759ccc00540769
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2024-03-25 12:38:26 -0500

    wtm states now have their own pages; create states store

--- src/routes/wtm/+page.svelte
+++ src/routes/wtm/+page.svelte
@@ -1,6 +1,8 @@
 <script>
+	import { statesOfAmericaInventory } from './unitedStatesStore';
 	import { eatAndDrinkInventory } from './eatAndDrinkStore';
 	import { entertainmentInventory } from './entertainmentStore';
+	import './wtm.css';
 </script>
 
 <div class="container">
@@ -13,180 +15,19 @@
 			<span class="highlight highlight-3">things to do</span>.
 		</p>
 
+		<h2>United States of America 🇺🇸</h2>
+
 		<div class="tableOfContents">
-			<a class="contentItem" href="#Iowa">Iowa 🌽<sup>[1]</sup></a>
-			<a class="contentItem" href="#Minnesota">Minnesota ❄️<sup>[2]</sup></a>
-		</div>
-
-		<div class="states">
-			<div class="state">
-				<h2 id="Iowa">Iowa 🌽</h2>
-				<h3>Eat and Drink 😋</h3>
-				<div class="cards">
-					{#each $eatAndDrinkInventory as inventoryItem}
-						{#if inventoryItem.state == 'Iowa'}
-							<div class="card">
-								<div class="cardLabel">
-									<h4 class="cardName">{inventoryItem.name}</h4>
-									{#if inventoryItem.roots == 'Local'}
-										<span class="highlight highlight-2">{inventoryItem.roots}</span>
-									{:else}
-										<span class="highlight highlight-3">{inventoryItem.roots}</span>
-									{/if}
-								</div>
-								<ul>
-									<li>{inventoryItem.cuisine}</li>
-									<li>{inventoryItem.address}</li>
-									<li><a href={inventoryItem.website}>{inventoryItem.website}</a></li>
-									<li>{inventoryItem.phone}</li>
-								</ul>
-							</div>
-						{/if}
-					{/each}
-				</div>
-				<h3>Entertainment 🍿</h3>
-				<div class="cards">
-					{#each $entertainmentInventory as inventoryItem}
-						{#if inventoryItem.state == 'Iowa'}
-							<div class="card">
-								<div class="cardLabel">
-									<h4 class="cardName">{inventoryItem.name}</h4>
-									{#if inventoryItem.roots == 'Local'}
-										<span class="highlight highlight-2">{inventoryItem.roots}</span>
-									{:else}
-										<span class="highlight highlight-3">{inventoryItem.roots}</span>
-									{/if}
-								</div>
-								<ul>
-									<li>{inventoryItem.type}</li>
-									<li>{inventoryItem.address}</li>
-									<li><a href={inventoryItem.website}>{inventoryItem.website}</a></li>
-									<li>{inventoryItem.phone}</li>
-								</ul>
-							</div>
-						{/if}
-					{/each}
-				</div>
-			</div>
-		</div>
-
-		<div class="states">
-			<div class="state">
-				<h2 id="Minnesota">Minnesota ❄️</h2>
-				<h3>Eat and Drink 😋</h3>
-				<div class="cards">
-					{#each $eatAndDrinkInventory as inventoryItem}
-						{#if inventoryItem.state == 'Minnesota'}
-							<div class="card">
-								<div class="cardLabel">
-									<h4 class="cardName">{inventoryItem.name}</h4>
-									{#if inventoryItem.roots == 'Local'}
-										<span class="highlight highlight-2">{inventoryItem.roots}</span>
-									{:else}
-										<span class="highlight highlight-3">{inventoryItem.roots}</span>
-									{/if}
-								</div>
-								<ul>
-									<li>{inventoryItem.cuisine}</li>
-									<li>{inventoryItem.address}</li>
-									<li><a href={inventoryItem.website}>{inventoryItem.website}</a></li>
-									<li>{inventoryItem.phone}</li>
-								</ul>
-							</div>
-						{/if}
-					{/each}
-				</div>
-				<h3>Entertainment 🍿</h3>
-				<div class="cards">
-					{#each $entertainmentInventory as inventoryItem}
-						{#if inventoryItem.state == 'Minnesota'}
-							<div class="card">
-								<div class="cardLabel">
-									<h4 class="cardName">{inventoryItem.name}</h4>
-									{#if inventoryItem.roots == 'Local'}
-										<span class="highlight highlight-2">{inventoryItem.roots}</span>
-									{:else}
-										<span class="highlight highlight-3">{inventoryItem.roots}</span>
-									{/if}
-								</div>
-								<ul>
-									<li>{inventoryItem.type}</li>
-									<li>{inventoryItem.address}</li>
-									<li><a href={inventoryItem.website}>{inventoryItem.website}</a></li>
-									<li>{inventoryItem.phone}</li>
-								</ul>
-							</div>
-						{/if}
-					{/each}
-				</div>
-			</div>
+			{#each $statesOfAmericaInventory as state}
+				{#if state.enabled == 'true'}
+					<a class="contentItem" href="/wtm/{state.name.toLowerCase()}"
+						>{state.name} {state.emoji}</a
+					>
+				{/if}
+			{/each}
 		</div>
 	</div>
 	<div class="margin">
-		<p><sub><sup>[1]</sup>This is not a generalization.</sub></p>
-		<p><sub><sup>[2]</sup>This is also not a generalization.</sub></p>
+		<p><sub><sup>[0]</sup>The States' emojis are not a generalization. <i>Mostly</i>.</sub></p>
 	</div>
 </div>
-
-<style>
-	.tableOfContents {
-		display: flex;
-		gap: var(--gap);
-		margin-bottom: var(--margin);
-	}
-
-	.tableOfContents a {
-		text-decoration: none;
-	}
-
-	.contentItem {
-		border: var(--border);
-		border-radius: var(--border-radius);
-		padding: var(--padding-small);
-	}
-
-	.states {
-		border-top: var(--border);
-	}
-
-	.state {
-		/* border-bottom: var(--border); */
-		padding-bottom: var(--padding);
-	}
-
-	.cards {
-		display: grid;
-		/* flex-direction: row;
-		flex-wrap: wrap; */
-		gap: var(--gap);
-	}
-
-	@media (min-width: 760px) {
-		.cards {
-			/* flex-wrap: nowrap; */
-			grid-template-columns: repeat(2, 1fr);
-		}
-	}
-
-	.card {
-		padding: var(--padding);
-		border: var(--border);
-		border-radius: var(--border-radius);
-		/* flex-grow: 1; */
-	}
-
-	.card h4,
-	ul {
-		margin-top: 0;
-		margin-bottom: 0;
-	}
-
-	.cardLabel {
-		display: flex;
-		align-items: center;
-	}
-
-	.cardName {
-		flex-grow: 1;
-	}
-</style>
---
+++ src/routes/wtm/iowa/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+	import { eatAndDrinkInventory } from '../eatAndDrinkStore';
+	import { entertainmentInventory } from '../entertainmentStore';
+</script>
+
+<div class="container">
+	<div class="content">
+		<h1>🧭 What's the Move for Iowa 🌽</h1>
+
+		<p>
+			A curated selection of <span class="highlight highlight-3">where to eat</span>,
+			<span class="highlight highlight-3">places to see</span>, and
+			<span class="highlight highlight-3">things to do</span>.
+		</p>
+
+		<div class="tableOfContents">
+			<a class="contentItem" href="/wtm">👈 Back</a>
+		</div>
+
+		<div class="states">
+			<div class="state">
+				<h2 id="Iowa">Iowa 🌽</h2>
+				<h3>Eat and Drink 😋</h3>
+				<div class="cards">
+					{#each $eatAndDrinkInventory as inventoryItem}
+						{#if inventoryItem.state == 'Iowa'}
+							<div class="card">
+								<div class="cardLabel">
+									<h4 class="cardName">{inventoryItem.name}</h4>
+									{#if inventoryItem.roots == 'Local'}
+										<span class="highlight highlight-2">{inventoryItem.roots}</span>
+									{:else}
+										<span class="highlight highlight-3">{inventoryItem.roots}</span>
+									{/if}
+								</div>
+								<ul>
+									<li>{inventoryItem.cuisine}</li>
+									<li>{inventoryItem.address}</li>
+									<li><a href={inventoryItem.website}>{inventoryItem.website}</a></li>
+									<li>{inventoryItem.phone}</li>
+								</ul>
+							</div>
+						{/if}
+					{/each}
+				</div>
+				<h3>Entertainment 🍿</h3>
+				<div class="cards">
+					{#each $entertainmentInventory as inventoryItem}
+						{#if inventoryItem.state == 'Iowa'}
+							<div class="card">
+								<div class="cardLabel">
+									<h4 class="cardName">{inventoryItem.name}</h4>
+									{#if inventoryItem.roots == 'Local'}
+										<span class="highlight highlight-2">{inventoryItem.roots}</span>
+									{:else}
+										<span class="highlight highlight-3">{inventoryItem.roots}</span>
+									{/if}
+								</div>
+								<ul>
+									<li>{inventoryItem.type}</li>
+									<li>{inventoryItem.address}</li>
+									<li><a href={inventoryItem.website}>{inventoryItem.website}</a></li>
+									<li>{inventoryItem.phone}</li>
+								</ul>
+							</div>
+						{/if}
+					{/each}
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="margin">
+		<p><sub><sup>[0]</sup>The States' emojis are not a generalization. <i>Mostly</i>.</sub></p>
+	</div>
+</div>
