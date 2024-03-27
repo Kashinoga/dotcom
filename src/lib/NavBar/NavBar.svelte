072074ae3d76f0dff9ee240ba32bfb191dc3547a
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2024-03-27 18:28:09 -0500

    added clearing for fab button; updated nav spacing

--- src/lib/NavBar/NavBar.svelte
+++ src/lib/NavBar/NavBar.svelte
@@ -4,15 +4,13 @@
 
 <nav>
 	<div class="nav-items">
-		<div class="nav-buttons">
-			<button>
-				<a href="/" data-sveltekit-replaceState>💖 Kashinoga</a>
-			</button>
-			<ThemeSwitcher></ThemeSwitcher>
-			<button>
-				<a href="/menu" data-sveltekit-replaceState>🍔 Menu</a>
-			</button>
-		</div>
+		<button>
+			<a href="/" data-sveltekit-replaceState>💖 Kashinoga</a>
+		</button>
+		<ThemeSwitcher></ThemeSwitcher>
+		<button>
+			<a href="/menu" data-sveltekit-replaceState>🍔 Menu</a>
+		</button>
 	</div>
 </nav>
 
@@ -21,18 +19,27 @@
 		font-family: var(--font-family);
 	}
 
+	nav {
+		/* position: sticky; */
+		/* top: var(--margin); */
+		background-color: var(--background-color);
+	}
+
 	@media (min-width: 900px) {
 		nav {
+			position: unset;
 			max-width: var(--width-large);
+			border: 0;
+			border-radius: 0;
 			border-bottom: var(--border);
 		}
 	}
 
 	.nav-items {
-		width: auto;
-		justify-content: center;
+		justify-content: space-evenly;
 		padding: var(--padding-small);
 		display: flex;
+		gap: var(--gap-small);
 		border: var(--border);
 		border-radius: var(--border-radius);
 	}
@@ -45,6 +52,7 @@
 			padding-left: 0;
 			padding-right: 0;
 			border: 0;
+			border-radius: 0;
 		}
 	}
 
