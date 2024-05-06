1dfb56d1d87ec8646f7d5ddfd0e3e17d173697e2
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2024-05-06 10:12:13 -0500

    add viewport meta tag for webkit fix, maybe, hopefully

--- src/routes/constellation/create-a-character/+page.svelte
+++ src/routes/constellation/create-a-character/+page.svelte
@@ -1,7 +1,27 @@
+<script>
+	import MarginNav from '$lib/Margin/MarginNav.svelte';
+</script>
+
 <div class="container">
 	<div class="content">
-		<h1 >🥚 Create a Character</h1>
+		<h1>🥚 Create a Character</h1>
 		<p>Coming soon.</p>
+
+		<h2>1. Choose a Race</h2>
+
+		<h2>2. Choose a Background</h2>
+
+		<h2>3. Choose a Class</h2>
+
+		<h2>4. Determine Abilities</h2>
+
+		<h2>5. Determine Skills</h2>
 	</div>
-	<div class="margin"></div>
+	<div class="margin">
+		<div class="marginPills">
+			<div class="navPills">
+				<MarginNav></MarginNav>
+			</div>
+		</div>
+	</div>
 </div>
