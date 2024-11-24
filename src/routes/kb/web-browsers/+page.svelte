b694c251b9e59c33107556a0d714490f1dabd77b
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2024-11-24 17:36:36 -0600

    fix overflow for kb cards

--- src/routes/kb/web-browsers/+page.svelte
+++ src/routes/kb/web-browsers/+page.svelte
@@ -6,32 +6,33 @@
 	<div class="content">
 		<div class="sections">
 			<div class="section">
-				<h2>🕸️ Web Browsers</h2>
-				<p>You've got <span class="highlight highlight-blue">options</span>!</p>
+				<div class="title">
+					<div class="title-emoji"><h2>🕸️</h2></div>
+					<div class="title-text">
+						<h2>Web Browsers</h2>
+						<h3>You've got <span class="highlight highlight-blue">options</span>!</h3>
+					</div>
+				</div>
 				<div class="cardsContainer">
-					{#each $categories as category}
-						<h3>
-							{category.name}
-						</h3>
-						<div class="cards">
-							<div class="card">
-								<div class="cardInfo">
-									<h4 class="cardName">Microsoft Edge is managed by your organization</h4>
-									<p>It's not, really.</p>
-									<ol>
-										<li>Go to: edge://policy and review if there are actual policies</li>
-										<li>If there are none, continue</li>
-										<li>Close Edge</li>
-										<li>Open regedit</li>
-										<li>Navigate to: HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge</li>
-										<li>Delete this directory</li>
-										<li>Open Edge and verify</li>
-									</ol>
-								</div>
+					<h3>Microsoft Edge</h3>
+					<div class="cards">
+						<div class="card">
+							<div class="cardInfo">
+								<h4 class="cardName">Microsoft Edge is managed by your organization</h4>
+								<p>It's not, really.</p>
+								<ol>
+									<li>Go to: edge://policy and review if there are actual policies</li>
+									<li>If there are none, continue</li>
+									<li>Close Edge</li>
+									<li>Open regedit</li>
+									<li>Navigate to: HKEYd_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge</li>
+									<li>Delete this directory</li>
+									<li>Open Edge and verify</li>
+								</ol>
 							</div>
+							<div class="cardLabel">BUG</div>
 						</div>
-					{/each}
-					<div class="cards"></div>
+					</div>
 				</div>
 			</div>
 		</div>
