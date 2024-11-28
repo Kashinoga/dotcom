8f3b513bab16992a29f11b06923d49919a833132
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2024-11-28 14:05:48 -0600

    drawer updates

--- src/routes/inthewood/Drawer.svelte
+++ src/routes/inthewood/Drawer.svelte
@@ -1,6 +1,43 @@
-<script>
+<script lang="ts">
+	import { tick } from 'svelte';
 	import { playerInventory } from './playerInventoryStore';
-	export let isOpen = false;
+	import {
+		addRandomGatherMessageToSessionLog,
+		addRandomMessageToSessionLog
+	} from './sessionLogStore';
+
+	let { isOpen = $bindable() } = $props();
+
+	function delay(ms: number | undefined) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+
+	let container: HTMLDivElement;
+	function scrollToBottom() {
+		if (container) {
+			container.scrollTop = container.scrollHeight;
+		}
+	}
+
+	function handleClick() {
+		isDisabled = true; // Disable the button
+
+		setTimeout(() => {
+			isDisabled = false;
+		}, 1000);
+	}
+
+	let isDisabled = $state(false);
+	async function handsAction() {
+		addRandomMessageToSessionLog();
+		await tick(); // Wait for DOM to update
+		scrollToBottom();
+		handleClick();
+		await delay(1000);
+		addRandomGatherMessageToSessionLog();
+		await tick(); // Wait for DOM to update
+		scrollToBottom();
+	}
 </script>
 
 <!-- Drawer -->
@@ -11,6 +48,50 @@
 			<!-- {isOpen ? 'Close' : 'Open'} --> Player's Name
 		</button>
 	</div>
+
+	<div class="section section-100">
+		<div class="equipment">
+			<div class="equip">
+				<div class="title">Hands</div>
+				<div class="equip-actions">
+					<div class="actions">
+						<button class="action" onclick={handsAction} disabled={isDisabled}
+							>{isDisabled ? 'Gathering...' : 'Gather'}
+							<div class="loading-bar"></div></button
+						>
+						<button class="action">Hunt</button>
+					</div>
+					<div class="items">
+						<div class="item">
+							<select
+								><option disabled>Select a Left-Hand Item</option>
+								<optgroup label="Starting Items">
+									{#each $playerInventory as item}
+										<option>
+											{item.name}
+										</option>
+									{/each}
+								</optgroup>
+							</select>
+						</div>
+						<div class="item">
+							<select
+								><option disabled>Select a Right-Hand Item</option>
+								<optgroup label="Starting Items">
+									{#each $playerInventory as item}
+										<option>
+											{item.name}
+										</option>
+									{/each}
+								</optgroup>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<div class="drawer-content">
 		{#each $playerInventory as item}
 			<button>
@@ -101,5 +182,97 @@
 
 	.drawer-button:hover {
 		background-color: var(--blue-hover);
+	}
+
+	.equipment {
+		display: flex;
+		gap: var(--gap-small);
+		border-top: var(--border);
+		padding: var(--padding);
+		border: var(--border);
+		border-radius: var(--border-radius);
+		background-color: var(--background-color-glass);
+		backdrop-filter: var(--backdrop-filter-glass);
+	}
+
+	.equip {
+		display: flex;
+		flex-grow: 1;
+		flex-direction: column;
+	}
+
+	.equip-actions {
+		display: flex;
+		flex-direction: column;
+		gap: var(--gap);
+	}
+
+	@media (min-width: 900px) {
+		.equip-actions {
+			flex-direction: row;
+		}
+	}
+
+	.actions {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		flex-wrap: wrap;
+		gap: var(--gap-small);
+	}
+
+	@media (min-width: 900px) {
+		.actions {
+			flex-direction: row;
+			border-right: var(--border-dotted);
+		}
+	}
+
+	.action {
+		position: relative; /* Allows positioning of child elements */
+		cursor: pointer;
+		overflow: hidden; /* Ensures the loading bar stays inside the button */
+	}
+
+	@media (min-width: 900px) {
+		.action {
+			width: 8em;
+		}
+	}
+
+	.action:disabled {
+		cursor: not-allowed;
+		background-color: var(--blue-hover);
+	}
+
+	.loading-bar {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		height: 4px;
+		background: var(--green);
+		width: 0;
+		transition: width 1s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+	}
+
+	.action:disabled .loading-bar {
+		width: 100%;
+	}
+
+	.action:not(:disabled) .loading-bar {
+		transition: none; /* Instantly resets when button is re-enabled */
+		width: 0; /* Resets width immediately */
+	}
+
+	.items {
+		display: flex;
+		flex-direction: column;
+		gap: var(--gap-small);
+	}
+
+	@media (min-width: 900px) {
+		.items {
+			flex-direction: row;
+		}
 	}
 </style>
