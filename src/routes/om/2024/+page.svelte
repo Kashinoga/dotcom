58d3d0eef24a7affea0c56419adb73916935c694
Andrew Nguyen <45315255+Kashinoga@users.noreply.github.com>
2024-12-31 13:24:29 -0600

    Create +page.svelte

---
+++ src/routes/om/2024/+page.svelte
@@ -0,0 +1,65 @@
+ <div class="container">
+	<div class="content">
+		<div class="sections">
+			<div class="section">
+				<h2>My Cat and I Both Take Prozac</h2>
+				<div class="paragraphs">
+					<p>
+						I’m writing this to—on repeat—AURORA’s “To Be Alright”. I’m at Horizon Line Coffee, drinking their seasonal “Deep in the Willow” matcha. It’s cold, windy, and feels like 12 degrees.
+					</p>
+          <p>
+              This has been the longest year of my life.
+          </p>
+          <p>
+              This year, I—in no particular order:
+                <ul>
+                  <li>Traveled to 6 different states</li>
+                  <li>Went to Minnesota 4 times</li>
+                  <li>Attended 2 K-Pop idol concerts</li>
+                  <li>Earned 5 hapkido belts</li>
+                  <li>Landed in the top 0.001% of listeners for Charli XCX’s “360”</li>
+                  <li>Reached Diamond 3 in Marvel Rivals</li>
+                  <li>Went on 4 unique dates</li>
+                  <li>Saw 20 new movies</li>
+                  <li>Had IU’s “Lilac” ruined 1 time for me</li>
+                  <li>Had Chappell Roan’s “HOT TO GO!” ruined 2 times for me</li>
+                  <li>Saw a live show of Whose Line is it Anyway for the 1st time</li>
+                  <li>Fell in love 2’wice</li>
+                  <li>Ate at 2 of Chicago’s best pizza joints</li>
+                  <li>Had 8 cups of watermelon at the Iowa State Fair</li>
+                  <li>Had 1 gas main explode outside of my apartment</li>
+                  <li>Saw my 1st aurora in person</li>
+                  <li>Went indoor skydiving for the 1st time</li>
+                  <li>Spent 12 hours beginning at 2am migrating my workplace’s frontline response system</li>
+                  <li>1’nce, went to New China 3 times in a week</li>
+                  <li>Committed code 160 times to my personal website</li>
+                  <li>Went 4 months without Prozac</li>
+                  <li>Missed 2 therapy sessions</li>
+                  <li>Reached at least Gold 5 in all roles in Overwatch</li>
+                  <li>Fell in love with 1 stripper</li>
+                  <li>Went to my 1st Vietnamese buffet</li>
+                  <li>Experienced love at 1st sight for the 1st time</li>
+                  <li>Had an anxiety attack 4 times</li>
+                  <li>Spent over 240 hours at Gray’s Lake</li>
+                  <li>Let my depression run its course 2’wice</li>
+                  <li>…and much, much more.</li>
+                </ul>
+              </p>
+              <p>I gave myself a little under an hour to come up with this list. In this time, I found it incredibly hard to                  think about and to remember the highlights of my year—without having to resort to my journal or my photos. I think it reinforces how important it is for me to relax and record from time to time.</p>
+
+<p>Those familiar with me may have noticed that I have shared less and less of these as the year has gone on. It’s much easier to sit down and write when I’m sad—it offers a sense of letting go of that sadness into the void. Perhaps this is an indicator that I’m doing much better this year.</p>
+
+<p>Those unfamiliar with me: I encountered major grief about two years ago. I threw myself into psychiatry and therapy, and I started to write and to share these letters with a small group of people. It’s a little embarrassing and cringey to go back and to read some of my earlier letters, but I definitely feel like expressing and sharing my innermost turmoils and jubilations has immensely helped me move forward with my grief.</p>
+
+<p>Even with the help that I’ve received, the tools and techniques that I’ve acquired and learned to use, and the emotional growth that I’ve nurtured this year: I still make mistakes and know I’ll continue to make them. Though, it’s no excuse to not keep growing, to seek genuine forgiveness, and to do markedly better. To be better. To be best. (Sorry, I’m going to keep sneaking that line in.)</p>
+
+<p>If you’re reading this, please know that you’ve helped me in some way this past year. I hope I’ve been able to give back in some useful way as a token of my appreciation.</p>
+
+<p>I hope the year has treated you fairly, and I wish you all the best in 2025.</p>
+
+<p>Stay safe. Look up.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
