81c2a368df5f3581119e18c77da321c764ba8408
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2025-02-15 20:20:20 -0600

    init release of Post

--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -6,14 +6,28 @@
 	<div class="content">
 		<div class="sections">
 			<div class="section">
-				<h1>📬 Post</h1>
-			</div>
-			<div class="section">
+				<div class="title">
+					<div class="title-emoji">
+						<h1>📬</h1>
+					</div>
+					<div class="title-text">
+						<h1>Post</h1>
+						<h3><i>A scroll of well-articulated thoughts.</i></h3>
+					</div>
+				</div>
+
+				<div class="paragraphs">
+					<p>
+						Post is my collection of writings that I'm willing to make public. I hope they offer
+						some useful or comedic insight into my perspective of life.
+					</p>
+				</div>
+
 				<ul>
 					{#each data.posts as post}
 						<li>
 							<a href="/post/{post.slug}">
-								{post.title} - {post.date}
+								{post.title}, {post.date}
 							</a>
 						</li>
 					{/each}
