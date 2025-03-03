3962dadad31a844500b7e0701706b2f1a160dacf
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2025-03-02 18:08:28 -0600

    Change Post sorting

--- src/routes/post/+page.ts
+++ src/routes/post/+page.ts
@@ -1,6 +1,19 @@
-export async function load({ fetch }) {
+// Sort posts by date in descending order
+interface Post {
+	date: string;
+	[key: string]: any;
+}
+
+export async function load({
+	fetch
+}: {
+	fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+}): Promise<{ posts: Post[] }> {
 	const res = await fetch('/posts/posts.json'); // Fetch list of posts from static folder
-	const posts = await res.json();
+	const posts: Post[] = await res.json();
+
+	// Sort posts by date in descending order
+	posts.sort((a: Post, b: Post) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
 	return { posts };
 }
