16856be405624b9b07776d8cd3964d13a8615c2f
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2025-03-25 16:29:50 -0500

    fixed menu links; updated menu styling towards cards styling

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,83 +1,103 @@
 <div class="container">
 	<div class="content">
 		<div class="sections">
-			<div class="section">
-				<a href="https://ko-fi.com/kashinoga">
+			<a href="https://ko-fi.com/kashinoga">
+				<div class="section">
 					<div class="title">
 						<div class="title-emoji">
 							<h1>💵</h1>
 						</div>
 						<div class="title-text">
 							<h1>Donate</h1>
-							<h2>Give me your money!</h2>
+							Give me your money!
 						</div>
 					</div>
-				</a>
-			</div>
+				</div>
+			</a>
 
-			<div class="section">
-				<a href="/om">
+			<a href="/om">
+				<div class="section">
 					<div class="title">
 						<div class="title-emoji">
 							<h1>🎤</h1>
 						</div>
 						<div class="title-text">
 							<h1>Open Mic</h1>
-							<h2>A place to open up.</h2>
+							A place to open up.
 						</div>
 					</div>
-				</a>
-			</div>
+				</div>
+			</a>
 
-			<div class="section">
-				<a href="/post">
+			<a href="/post">
+				<div class="section">
 					<div class="title">
 						<div class="title-emoji">
 							<h1>📬</h1>
 						</div>
 						<div class="title-text">
 							<h1>Post</h1>
-							<h2>A personal, curated collection of writings made public.</h2>
+							A personal, curated collection of writings made public.
 						</div>
 					</div>
-				</a>
-			</div>
+				</div>
+			</a>
 
-			<div class="section">
-				<a href="/post">
+			<a href="/wtm">
+				<div class="section">
 					<div class="title">
 						<div class="title-emoji">
 							<h1>🧭</h1>
 						</div>
 						<div class="title-text">
 							<h1>What's the Move</h1>
-							<h2>
-								A locally crowdsourced selection of where to eat, places to see, and things to do.
-							</h2>
+
+							A locally crowdsourced selection of where to eat, places to see, and things to do.
 						</div>
 					</div>
-				</a>
-			</div>
+				</div>
+			</a>
 
-			<div class="section">
-				<a href="/post">
+			<a href="/wx">
+				<div class="section">
 					<div class="title">
 						<div class="title-emoji">
 							<h1>🌦️</h1>
 						</div>
 						<div class="title-text">
 							<h1>WX</h1>
-							<h2>A simple weather app.</h2>
+							A simple weather app.
 						</div>
 					</div>
-				</a>
-			</div>
+				</div>
+			</a>
 		</div>
 	</div>
 </div>
 
 <style>
 	a {
+		display: flex;
+		flex-grow: 1;
 		text-decoration: none;
+	}
+
+	.sections {
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: unset;
+	}
+
+	.section {
+		display: flex;
+		flex-grow: 1;
+		border: var(--border-dotted);
+		border-radius: var(--border-radius);
+		padding: var(--padding);
+	}
+
+	.title {
+		border-bottom: 0;
+		padding-bottom: 0;
 	}
 </style>
